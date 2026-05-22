04ac6643732330f27de57ba5a743d198db914d14
Ashish RAj <rajshish37@gmail.com>
2026-05-22 16:26:48 +0530

    The DOM Manipulations in js

---
+++ The Dom/dom5_qn/Qn5.js
@@ -0,0 +1,12 @@
+let input =document.querySelector("input");
+
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+btn.onclick = ()=>{
+    let li = document.createElement("li");
+
+    li.innerText = input.value;
+    ul.appendChild(li);
+    input.value = " ";
+}
